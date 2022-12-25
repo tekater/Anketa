eebf92f2a1eb33bd80bd461d1398493ab46eb6be
tekater <120014627+tekater@users.noreply.github.com>
2022-12-26 00:05:06 +0900

    Update Anketa.java

--- Anketa.java
+++ Anketa.java
@@ -1,12 +1,14 @@
-
 import java.util.Scanner;
 class Main {
 public static void main(String[] args){
 Scanner scanner = new Scanner(System.in);
-String name = "Anton";
-String exp = "10";
-String kitchen = "Sweet";
-String anketa = "Анкета заполнена " + name + " сохранено. Мы свяжемся с вами если будем нуждаться в услугах повора который готовит " + kitchen + " блюда и имеет " + exp + " лет опыта";
+System.out.println("Введите имя:");
+String name = scanner.nextLine();
+System.out.println("Введите опыт работы:");
+String exp = scanner.nextLine();
+System.out.println("Введите вашу кулинарию:");
+String kitchen = scanner.nextLine();
+String anketa = "Анкета заполнена " + name + " сохранено. Мы свяжемся с вами если будем нуждаться в услугах повора который готовит " + kitchen + " блюда и имеет " + exp + " лет опыта работы";
 System.out.println(anketa); 
 }
 } 
