f8fa36f780731a9924c0fbf638504895618d507a
tekater <120014627+tekater@users.noreply.github.com>
2022-12-25 23:16:38 +0900

    Add files via upload

---
+++ anketa.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+class Main {
+public static void main(String[] args){
+Scanner scanner = new Scanner(System.in);
+String name = "Anton";
+String exp = "10";
+String kitchen = "Sweet";
+String anketa = "Анкета заполнена " + name + " сохранено. Мы свяжемся с вами если будем нуждаться в услугах повора который готовит " + kitchen + " блюда и имеет " + exp + " лет опыта";
+System.out.println(anketa); 
+}
+} 
